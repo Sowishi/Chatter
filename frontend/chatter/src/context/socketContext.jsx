09c474abf6b34b9b74f0d0af0ebc97cc7b3965b4
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-06-07 19:21:34 +0800

    socket url updated

--- frontend/chatter/src/context/socketContext.jsx
+++ frontend/chatter/src/context/socketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chatter-o093.onrender.com", {
         query: {
           userID: authUser._id,
         },
