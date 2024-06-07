dd55e45845cc860cab3a2513f8a470100bdd43b6
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-06-07 18:38:51 +0800

    Connected backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
@@ -10,12 +11,22 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/chatter/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "frontend", "chatter", "dist", "index.html")
+  );
+});
 
 server.listen(PORT, () => {
   connectToMongo();
